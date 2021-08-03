3536be3a01c21cc0b34bc2fdc08db81d289d8a9d
Валентин <123qwemmm@mail.ru>
2021-08-03 18:19:55 +0300

    инсту перенес в главный проект постопус

--- instagram.py
+++ instagram.py
@@ -6,13 +6,25 @@ from instabot import Bot
 
 from logpass.logpass import insta_mi_l, insta_mi_p
 
-path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config')
-if os.path.exists(path):
-    shutil.rmtree(path)
+dir_del = ('config', 'out')
 
-photo = 'metabo.jpg'
-caption = "Шуруповерты в магазине БРИГАДИР, г.Малмыж, ул.Урицкого, 3 (в здании Сбербанка)"
+for i in dir_del:
+    for filename in os.listdir(i):
+        file_path = os.path.join(i, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
+
+caption = "🔔Продам скоростной велосипед. Цена 11000 руб. Торг. Тел 89822141847" \
+          " -> https://vk.com/wall-149841761_78984 #ОбъявленияМалмыж Нажми лайк ❤ и поделись новостью с друзьями 👇"
+
+
+photo = 'in/1.jpg'
 im = Image.new('RGB', (1080, 1080), color='white')
 
 tatras = Image.open(photo)
@@ -35,13 +47,14 @@ draw_text = ImageDraw.Draw(im)
 font = ImageFont.truetype("arial.ttf", size=18)
 draw_text.text((10, 10), 'Малмыж Инфо', font=font)
 
-im.save('image.jpeg')
+im.save('out/1.jpeg')
+
 
 bot = Bot()
 bot.login(username=insta_mi_l, password=insta_mi_p)
 
 #  upload a picture
-bot.upload_photo('image.jpeg', caption=caption)
+bot.upload_photo('out/1.jpeg', caption=caption)
 
 '''from instabot import Bot
 bot = Bot()
@@ -83,3 +96,35 @@ img = Image.open('pic.png') #открываем изображение
 format = img.format #формат изображения
 size = img.size #размер изображения
 histogram = image.histogram() # получаем гистограмму'''
+
+# Класная штука, перебор файлов в папке и работа с ними
+'''photo_out = []
+for i in range(10):
+    if os.path.exists('in/' + str(i + 1) + '.jpg'):
+        photo = 'in/' + str(i + 1) + '.jpg'
+        im = Image.new('RGB', (1080, 1080), color='white')
+
+        tatras = Image.open(photo)
+        width, height = tatras.size
+        new_height = 1080  # Высота
+        new_width = int(new_height * width / height)
+        tatras = tatras.resize((new_width, new_height), Image.ANTIALIAS)
+        width, height = tatras.size
+        if width > 1080:
+            new_width = 680  # ширина
+            new_height = int(new_width * height / width)
+            tatras = tatras.resize((new_width, new_height), Image.ANTIALIAS)
+
+        width, height = tatras.size
+        koordinat = int((1080 - width) / 2)
+
+        im.paste(tatras, (koordinat, 0))
+
+        draw_text = ImageDraw.Draw(im)
+        font = ImageFont.truetype("arial.ttf", size=18)
+        draw_text.text((10, 10), 'Малмыж Инфо', font=font)
+
+        im.save('out/' + str(i + 1) + '.jpeg')
+        photo_out.append('out/' + str(i + 1) + '.jpeg')
+
+print(photo_out)'''
