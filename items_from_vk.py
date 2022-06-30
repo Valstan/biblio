4e14ac57586b8245736711fa8a065b7011cf386f
server <123qwemmm@mail.ru>
2022-07-01 00:34:46 +0300

    тестировал

--- items_from_vk.py
+++ items_from_vk.py
@@ -1,15 +1,17 @@
-from logpass.logpass import login_valstan, password_valstan
+import config
 from moduls.read_write.get_msg import get_msg
 from moduls.read_write.get_session_vk_api import get_session_vk_api
-from moduls.utils.clear_copy_history import clear_copy_history
+# from moduls.utils.clear_copy_history import clear_copy_history
 
-vkapp = get_session_vk_api(login_valstan, password_valstan)
-new_posts = get_msg(vkapp, -158787639, 0, 20)
-sample_clear = []
-for sample in new_posts:
-    template = clear_copy_history(vkapp, sample)
-    if template['owner_id'] not in (-179037590, -162751110):
-        sample_clear.append(template)
+session = config.session
+
+vkapp = get_session_vk_api(session['VK_LOGIN_VALSTAN'], session['VK_PASSWORD_VALSTAN'])
+new_posts = get_msg(vkapp, -43215063, 0, 20)
+# sample_clear = []
+# for sample in new_posts:
+#     template = clear_copy_history(vkapp, sample)
+#     if template['owner_id'] not in (-179037590, -162751110):
+#         sample_clear.append(template)
 pass
 print('Конец')
 
