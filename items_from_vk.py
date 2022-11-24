4418746f42bc3b6311c0d5c29d41209fc98f716e
Валентин <123qwemmm@mail.ru>
2022-11-24 08:45:46 +0300

    установил графический гит
    пробую через него

--- items_from_vk.py
+++ items_from_vk.py
@@ -7,7 +7,7 @@ session = config.session
 
 
 vkapp = get_session_vk_api(session['VK_TOKEN_VALSTAN'])
-new_posts = get_msg(vkapp, -155169029, 0, 20)
+new_posts = get_msg(vkapp, -9363816, 0, 20)
 sample_clear = []
 for sample in new_posts:
     template = clear_copy_history(sample)
